8c1fb2c2926a677055126fa632eb6be130d1dace
FATIMAT <FATIMAT@ATNSW-BENCH012.dialoggroup.internal>
2016-05-10 13:09:22 +1000

    aaa

--- src/home/DKTNew.java
+++ src/home/DKTNew.java
@@ -32,10 +32,7 @@ public class DKTNew extends PerfDB {
 	public void DKT() throws InterruptedException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
 	//public static void main() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, InterruptedException{
 		
-		System.out.println("First check");
-		System.out.println("jlkjkj");
-		System.out.println("kjkjk");
-			
+		
 		WebDriver driver;
 		String expStr1 = "Customer Charter";
 		String expStr2 = "Do it online";
